16117312012094633edabf9b7ea9117437a5665b
badhax <odain.chevannes@gmail.com>
2021-04-11 14:11:11 -0500

    meh

--- nse_test.py
+++ nse_test.py
@@ -1,11 +1,14 @@
 import requests
 import schedule
 import sys
+import datetime
 import time
+from datetime import timedelta
 from googleSheetsUtil import GoogleSheetsUtil
 
 api_endpoint = 'https://www.nseindia.com/api/option-chain-'
 webpage = 'https://www.nseindia.com/get-quotes/derivatives'
+
 
 def get_symbol(name,instrument='indices'):
 	print('updating {0} sheet...'.format(name))
@@ -41,21 +44,25 @@ def get_symbol(name,instrument='indices'):
 	
 def job():
 	while True:
-		job()
-		get_symbol('NIFTY')
-		get_symbol('BANKNIFTY')
-		get_symbol('RELIANCE','equities')
-		time.sleep(int(sys.argv[1])*60)
+		now = datetime.datetime.now()
+		end = now.replace(hour=13,minute=30,second=0,microsecond=0)
+		if now < end:
+			get_symbol('NIFTY')
+			get_symbol('BANKNIFTY')
+			get_symbol('RELIANCE','equities')
+			time.sleep(int(sys.argv[1])*60)
+		else:
+			break
 		
 if(len(sys.argv) < 3):
 	print('useage: nse_test.py interval_in_minutes google_sheet_id')
 else:
-	#job()
-	schedule.every().monday.at('09:00').until('13:30').do(job)
-	schedule.every().tuesday.at('09:00').until('13:30').do(job)
-	schedule.every().wednesday.at('09:00').until('13:30').do(job)
-	schedule.every().thursday.at('09:00').until('13:30').do(job)
-	schedule.every().friday.at('09:00').until('13:30').do(job)
+	job()
+	schedule.every().monday.at('09:00').do(job)
+	schedule.every().tuesday.at('09:00').do(job)
+	schedule.every().wednesday.at('09:00').do(job)
+	schedule.every().thursday.at('09:00').do(job)
+	schedule.every().friday.at('09:00').do(job)
 		
 	while True:
 		schedule.run_pending()
