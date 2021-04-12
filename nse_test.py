47493685d51766e471addcaf2f7f98d08b5ed0ea
badhax <odain.chevannes@gmail.com>
2021-04-12 11:29:57 -0500

    added logging for debugging purposes

--- nse_test.py
+++ nse_test.py
@@ -5,7 +5,9 @@ import datetime
 import time
 from datetime import timedelta
 from googleSheetsUtil import GoogleSheetsUtil
+import logging
 
+logging.basicConfig(level=logging.DEBUG)
 api_endpoint = 'https://www.nseindia.com/api/option-chain-'
 webpage = 'https://www.nseindia.com/get-quotes/derivatives'
 
@@ -39,7 +41,7 @@ def get_symbol(name,instrument='indices'):
 			
 	gs = GoogleSheetsUtil(sys.argv[2])
 	gs.add_row_range(all_data, name,2)
-	print("next update in {0} minutes".format(sys.argv[1]))
+	
 	
 	
 def job(force=False):
@@ -50,19 +52,28 @@ def job(force=False):
 			get_symbol('NIFTY')
 			get_symbol('BANKNIFTY')
 			get_symbol('RELIANCE','equities')
+			
+			if(force):
+				print("First run done. next execution: {0}".format(schedule.next_run()))
+				break
+				
+			print("next update in {0} minutes".format(sys.argv[1]))
 			time.sleep(int(sys.argv[1])*60)
 		else:
+			print("job finished for day. next execution: {0}".format(schedule.next_run()))
 			break
 		
 if(len(sys.argv) < 3):
 	print('useage: nse_test.py interval_in_minutes google_sheet_id')
 else:
-	job(force=True)
-	schedule.every().monday.at('09:00').do(job)
+	schedule.every().monday.at('9:00').do(job)
 	schedule.every().tuesday.at('09:00').do(job)
 	schedule.every().wednesday.at('09:00').do(job)
 	schedule.every().thursday.at('09:00').do(job)
 	schedule.every().friday.at('09:00').do(job)
+	
+	# first run to manually test if sheet is being updated
+	job(force=True)
 		
 	while True:
 		schedule.run_pending()
